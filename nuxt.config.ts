d422145894189cb494e83ae6746cf891d9e23267
Mateusz Kalinowski <mateuszkalin1@gmail.com>
2026-03-06 18:44:45 +0100

    chore: :arrow_up: Upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2026-03-01',
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
@@ -8,7 +8,9 @@ export default defineNuxtConfig({
     '@nuxt/test-utils'
   ],
   css: ['~/assets/main.css'],
-  routeRules: {
-    '/': { prerender: true }
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
   }
 })
